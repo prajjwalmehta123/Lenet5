daf4d8d69beac4b1e6a1adf881a46f4052c3e3aa
Aakriti Lnu <al1745@rit.edu>
2024-11-26 00:18:18 -0500

    test dataset run

--- main.cpp
+++ main.cpp
@@ -28,24 +28,67 @@ int main()
                 std::cerr << "Error: Environment variable " << mnist_label_var << " is not set!" << std::endl;
                 return 1;
         }
-        dataloader dataloader(mnist_image_path,mnist_label_path,256, false);
+        std::string mnist_test_image_path;
+        std::string mnist_test_label_path;
+        // Define the name of the environment variable
+        const std::string mnist_test_images_var = "MNIST_TEST_IMAGES_PATH";
+        const char* test_env_var_value_cstr = std::getenv(mnist_test_images_var.c_str());
+        if (test_env_var_value_cstr) {
+                mnist_test_image_path = std::string(test_env_var_value_cstr);
+                std::cout << "Test Images path: " << mnist_test_image_path << std::endl;
+        } else {
+                // Handle the case where the environment variable is not set
+                std::cerr << "Error: Test Environment variable " << mnist_test_images_var << " is not set!" << std::endl;
+                return 1;
+        }
+        const std::string mnist_test_label_var = "MNIST_TEST_LABELS_PATH";
+        test_env_var_value_cstr = std::getenv(mnist_test_label_var.c_str());
+        if (test_env_var_value_cstr) {
+                mnist_test_label_path = std::string(test_env_var_value_cstr);
+                std::cout << "Test Images path: " << mnist_test_label_path << std::endl;
+        } else {
+                // Handle the case where the environment variable is not set
+                std::cerr << "Error: Test Environment variable " << mnist_test_label_var << " is not set!" << std::endl;
+                return 1;
+        }
+        #ifdef USE_CUDA
+        dataloader dataloader_train(mnist_image_path,mnist_label_path,256, false);
+        #else
+         dataloader dataloader_train(mnist_image_path,mnist_label_path,64, false);
+        #endif
+       
         LeNet5 lenet;
-        int correct = 0;
+        
         auto start = std::chrono::high_resolution_clock::now();
         for(int epoch = 1; epoch <= 10; ++epoch) {
-                for(int i = 0; i<dataloader.num_batches;i++) {
-                        auto x =dataloader.get_batch();
-                        std::cout<<i<<": ";
-                        int batch_correct  = lenet.Forward_Propagation(x.first, x.second);
+                for(int i = 0; i<dataloader_train.num_batches;i++) {
+                        auto x =dataloader_train.get_batch();
+                        lenet.Forward_Propagation(x.first, x.second);
                         lenet.Back_Propagation(x.second);
-                        correct = batch_correct+correct;
+                        if (i % 50 == 0) {
+                                std::cout << "Epoch " << epoch << ", Step " << i 
+                                        << " - Loss: " << lenet.compute_loss(x.second) << std::endl;
+                        }
+                        // break;
                 }
-                float accuracy = float(correct)/60000;
-                std::cout<<"Accuracy for this epoch: "<<accuracy * 100<<std::endl;
+                
                 auto end = std::chrono::high_resolution_clock::now();
                 auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-                // Print the duration in milliseconds
-                std::cout << "Time taken: " << duration.count() << " milliseconds" << std::endl;
+                std::cout << "Time taken: " << duration.count()/1000 << " seconds" << std::endl;
+                dataloader_train.reset();
+                break;
         }
-        
+        int correct = 0;
+        dataloader dataloader_test(mnist_test_image_path,mnist_test_label_path,16, false);
+        for(int i = 0; i<dataloader_test.num_batches;i++) {
+                        auto x =dataloader_test.get_batch();
+                        lenet.Forward_Propagation(x.first, x.second);
+                        correct+=lenet.compute_accuracy(x.second);
+                }
+        float accuracy = float(correct)/10000;
+        std::cout<<"Accuracy for Test : "<<accuracy * 100<<std::endl;
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << "Time taken: " << duration.count()/1000 << " seconds" << std::endl;
 }
+
